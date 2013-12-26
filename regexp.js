6b477b0196e29ba2254f01e9b099c1a5921eba60
Fábio Santos <fabiosantosart@gmail.com>
2013-12-26 23:54:40 +0000

    first commit

--- regexp.js
+++ regexp.js
@@ -3,13 +3,14 @@
         return ;
     }
 
-    verboseRegExp.makeTokenizer = function () {
+    verboseRegExp.makeTokenizer = function (forStr) {
         var tokens = [
+            ['(\\d+)', 'numb'],
             ['(\\w+)', 'atom'],
+            ['(;)', 'sep'],
             ['(\'\\w+\'|"\\w+")', 'str'],
             ['(\\()', 'paren'],
-            ['(\\))', 'cparen'],
-            ['(;)', 'sep']
+            ['(\\))', 'cparen']
         ];
         var reStr = '',
             tokenNames = [],
@@ -19,12 +20,62 @@
             tokenNames[i + 1] = tokens[i][1];
             tokenIndices[tokens[i][1]] = i + 1;
         }
-        parser = new RegExp(reStr, 'g');
-        return {
-            re: parser,
-            names: tokenNames,
-            indices: tokenIndices
+        function Token(type, text) {
+            this.type = type;
+            this.text = text;
+        }
+        Token.fromGroups = function (groups) {
+            var currentType,
+                currentText;
+            for (var t = 1; t < groups.length; t += 1) {
+                if (groups[t]) {
+                    currentType = tokenNames[t];
+                    currentText = groups[t];
+                    break;
+                }
+            }
+            return new Token(currentType, currentText);
         };
+        var tokenizer = {
+            re: null,
+            names: tokenNames,
+            indices: tokenIndices,
+            fed: null,
+            queue: [],
+            history: [],
+            feed: function (fed) {
+                this.re = new RegExp(reStr, 'g');
+                this.fed = fed;
+            },
+            peek: function () {
+                if (this.queue.length) return this.queue[0];
+                var next = Token.fromGroups(
+                    this.re.exec(this.fed))
+                this.queue.unshift(next);
+                return next;
+            },
+            advance: function () {
+                var groups,
+                    tok;
+                if (this.queue.length) {
+                    tok = this.queue.splice(1)[0];
+                } else {
+                    groups = this.re.exec(this.fed);
+                    tok = Token.fromGroups(groups);
+                }
+                this.history.push(tok);
+                return tok;
+            }
+        };
+        tokenizer.feed(forStr || null);
+        return tokenizer;
+    };
+
+    verboseRegExp.parse = function (str) {
+        var tokenizer = verboseRegExp.makeTokenizer();
+        var names = tokenizer.names;
+        var re = tokenizer.re;
+        var i = tokenizer.indices;
     };
 
     exportFunc('verboseRegExp', verboseRegExp);
--- test/test.js
+++ test/test.js
@@ -12,20 +12,58 @@ function equal(a, b, msg) {
 }
 
 describe('parser', function () {
-    it('Its tokenizer regexp', function () {
-        var tokenizer = verboseRegExp.makeTokenizer()
-        var toks = tokenizer.re;
-        var n = tokenizer.indices;
-        var str = 'f1(ftwo(3; "str"))';
-        equal(toks.exec(str)[n.atom], 'f1')
-        equal(toks.exec(str)[n.paren], '(')
-        equal(toks.exec(str)[n.atom], 'ftwo')
-        equal(toks.exec(str)[n.paren], '(')
-        equal(toks.exec(str)[n.atom], '3')
-        equal(toks.exec(str)[n.sep], ';')
-        equal(toks.exec(str)[n.str], '"str"')
-        equal(toks.exec(str)[n.cparen], ')')
-        equal(toks.exec(str)[n.cparen], ')')
-        equal(toks.exec(str), null);
+    describe('tokenizer', function () {
+        it('regexp', function () {
+            var tokenizer = verboseRegExp.makeTokenizer()
+            var toks = tokenizer.re;
+            var n = tokenizer.indices;
+            var str = 'f1(ftwo(3; "str"))';
+            equal(toks.exec(str)[n.atom], 'f1')
+            equal(toks.exec(str)[n.paren], '(')
+            equal(toks.exec(str)[n.atom], 'ftwo')
+            equal(toks.exec(str)[n.paren], '(')
+            equal(toks.exec(str)[n.numb], '3')
+            equal(toks.exec(str)[n.sep], ';')
+            equal(toks.exec(str)[n.str], '"str"')
+            equal(toks.exec(str)[n.cparen], ')')
+            equal(toks.exec(str)[n.cparen], ')')
+            equal(toks.exec(str), null);
+        });
+        it('advances tokens', function () {
+            var tokenizer = verboseRegExp.makeTokenizer()
+            tokenizer.feed('func 2 1');
+            var func = tokenizer.advance();
+            equal(func.type, 'atom');
+            equal(func.text, 'func');
+            tokenizer.advance();  // 2
+            equal(tokenizer.advance().text, '1');
+            try {
+                tokenizer.advance();
+                ok(false);
+            } catch (e) {
+                ok(true, 'raised ' + e + 'when finished with tokens');
+            }
+        });
+        it('peeks', function () {
+            var tokenizer = verboseRegExp.makeTokenizer();
+            tokenizer.feed('someatom 1 2 3');
+            tokenizer.advance();
+            equal(tokenizer.peek().text, '1');
+            equal(tokenizer.peek().text, '1');
+            tokenizer.advance();
+            equal(tokenizer.peek().text, '2');
+            equal(tokenizer.advance().text, '1');
+            equal(tokenizer.peek().text, '2');
+            tokenizer.advance(), tokenizer.advance();
+            equal(tokenizer.peek(), null);
+        });
+        it('looks back', function () {
+            var tokenizer = verboseRegExp.makeTokenizer();
+            tokenizer.feed('1 2 3');
+            tokenizer.advance();
+            equal(tokenizer.lookBack(), '1');
+            equal(tokenizer.lookBack(), '1');
+            equal(tokenizer.lookBack(2), null);
+        });
     });
 });
